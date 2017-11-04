cccb44e4a613a3fd753e04182a2f38c8873a10ee
Amir <amirhkarimi@gmail.com>
2017-11-04 12:47:00 -0400

    minor update

--- amir/paired_classifier.py
+++ amir/paired_classifier.py
@@ -54,7 +54,7 @@ batch_size = 100
 latent_dim_x_1 = 10
 latent_dim_x_2 = 10
 latent_dim_y = 10
-epochs = 100
+epochs = 200
 intermediate_dim = 500
 epsilon_std = 1.0
 learning_rate = 0.0003
@@ -238,15 +238,15 @@ class ACCURACY(Callback):
 
 accuracy = ACCURACY()
 
-# def scheduler(epoch):
-#     if epoch > 200:
-#         return float(0.0001)
-#     if epoch > 100:
-#         return float(0.0003)
-#     else:
-#         return float(0.001) # initial_lrate
+def scheduler(epoch):
+    # if epoch > 200:
+    #     return float(0.0001)
+    if epoch > 100:
+        return float(0.0003)
+    else:
+        return float(0.001) # initial_lrate
 
-# change_lr = LearningRateScheduler(scheduler)
+change_lr = LearningRateScheduler(scheduler)
 
 
 model_weights = pickle.load(open('paired_classifier' + str(latent_dim_y) + 'd_trained_on_' + dataset_name, 'rb'))
@@ -258,7 +258,7 @@ model.fit([x_train_1, x_train_2, y_train, y_train], [y_train, y_train],
         batch_size=batch_size,
         verbose=1,
         validation_data =([x_val_1, x_val_2, y_val, y_val], [y_val, y_val]),
-        callbacks = [accuracy])
+        callbacks = [accuracy, change_lr])
 
 model_weights = model.get_weights()
 pickle.dump((model_weights), open('paired_classifier' + str(latent_dim_y) + 'd_trained_on_' + dataset_name, 'wb'))
