79c861564809b49a76c3c935912fb25fd400b98c
Amir <amirhkarimi@gmail.com>
2017-11-04 14:13:45 -0400

    minor update

--- amir/main.py
+++ amir/main.py
@@ -54,7 +54,7 @@ batch_size = 100
 latent_dim_x_1 = 10
 latent_dim_x_2 = 10
 latent_dim_y = 10
-epochs = 100
+epochs = 50
 intermediate_dim = 500
 epsilon_std = 1.0
 learning_rate = 0.001
@@ -289,8 +289,8 @@ def vae_loss(x, _x_decoded):
     kl_loss_3 = - 0.5 * K.sum(1 + _z_log_var_2_1 - K.square(_z_mean_2_1) - K.exp(_z_log_var_2_1), axis=-1)
 #    kl_loss_4 = - 0.5 * K.sum(1 + _z_log_var_2_2 - K.square(_z_mean_2_2) - K.exp(_z_log_var_2_2), axis=-1)
     kl_loss_4 =  0.5 *  (K.sum(K.exp(_z_log_var_2_2)/K.exp(_z_log_var_1_2),axis = -1) + K.sum((_z_log_var_1_2- _z_mean_2_2)*(_z_log_var_1_2- _z_mean_2_2)/(K.exp(_z_log_var_1_2)),axis= -1 ) - latent_dim_y + K.sum(_z_log_var_1_2,axis=-1) -  K.sum(_z_log_var_2_2,axis=-1) )
-    y_loss_1 = 10 * objectives.categorical_crossentropy(yy_1, _y_decoded_1)
-    y_loss_2 = 100 * objectives.categorical_crossentropy(yy_2, _y_decoded_2)
+    y_loss_1 = 100 * objectives.categorical_crossentropy(yy_1, _y_decoded_1)
+    y_loss_2 = 1000 * objectives.categorical_crossentropy(yy_2, _y_decoded_2)
     return xent_loss_1 + xent_loss_2 + kl_loss_1 + kl_loss_2 + kl_loss_3 + kl_loss_4 + y_loss_1  + y_loss_2
 
 # model_1 = Model(inputs = [x_1,x_2,yy_1,yy_2], outputs = [_x_decoded_1,_x_decoded_2,_y_decoded_1,_y_decoded_2])
